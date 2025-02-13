1d89316f15ddc2b566a1503566e49487f0b8829d
Axel Donath <axel.donath@cfa.harvard.edu>
2025-02-13 13:50:14 -0500

    Add tests

--- tests/test_foo.py
+++
@@ -1,9 +0,0 @@
-from clouseau import inspector
-
-
-def test_jax():
-    pass
-
-def test_torch():
-    pass
-
---
+++ tests/test_inspector.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+import jax
+import jax.numpy as jnp
+from jax.tree_util import register_dataclass
+
+from clouseau import inspector, magnifier
+
+
+@register_dataclass
+@dataclass
+class Linear:
+    weight: jax.Array
+    bias: jax.Array
+
+    def __call__(self, x):
+        return jnp.dot(x, self.weight) + self.bias
+
+
+@register_dataclass
+@dataclass
+class SubModel:
+    linear: Linear
+
+    def __call__(self, x):
+        return self.linear(x)
+
+
+@register_dataclass
+@dataclass
+class Model:
+    sub_model: SubModel
+
+    def __call__(self, x):
+        return self.sub_model(x)
+
+def test_jax(tmp_path):
+    path = tmp_path / "trace.safetensors"
+    m = Model(SubModel(Linear(jnp.ones((2, 2)), jnp.ones(2))))
+
+    x = jnp.ones((2, 2))
+
+    with inspector(m, path, filter_=lambda _ : isinstance(_, Linear)) as fm:
+        fm(x)
+
+    magnifier(path)
+
+
+
+def test_torch():
+    pass
+
