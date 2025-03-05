4a35654b91c180bf3e9c32f44b75edbe1de78d5e
Axel Donath <axel.donath@cfa.harvard.edu>
2025-03-05 11:39:53 -0500

    Adapt torch test

--- tests/test_inspector.py
+++ tests/test_inspector.py
@@ -2,7 +2,9 @@ from dataclasses import dataclass
 
 import jax
 import jax.numpy as jnp
+import torch
 from jax.tree_util import register_dataclass
+from torch import nn as torch_nn
 
 from clouseau import inspector
 
@@ -35,6 +37,24 @@ class Model:
         return self.sub_model(x)
 
 
+class TorchSubModel(torch_nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = torch_nn.Linear(2, 2)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+class TorchModel(torch_nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sub_model = TorchSubModel()
+
+    def forward(self, x):
+        return self.sub_model(x)
+
+
 def test_jax(tmp_path):
     path = tmp_path / "trace.safetensors"
     m = Model(SubModel(Linear(jnp.ones((2, 2)), jnp.ones(2))))
@@ -50,6 +70,20 @@ def test_jax(tmp_path):
 
 def test_torch(tmp_path):
     path = tmp_path / "trace.safetensors"
+
+    m = TorchModel()
+
+    x = torch.ones((2, 2))
+
+    with inspector.tail(m, path, filter_=lambda p, _: isinstance(_, torch_nn.Linear)) as fm:
+        fm(x)
+
+    data = inspector.read_from_safetensors(path, framework="torch")
+    assert "sub_model.linear.forward" in data
+
+
+def test_equinox(tmp_path):
+    path = tmp_path / "trace.safetensors"
     m = Model(SubModel(Linear(jnp.ones((2, 2)), jnp.ones(2))))
 
     x = jnp.ones((2, 2))
@@ -60,7 +94,5 @@ def test_torch(tmp_path):
     data = inspector.read_from_safetensors(path, framework="jax")
     assert "sub_model.linear.__call__" in data
 
-    ...
 
 
-def test_equinox(): ...
