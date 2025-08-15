34512ed4ffddf9f70bcb98c4364fc788ee512a78
Axel Donath <axel.donath@cfa.harvard.edu>
2025-08-15 12:53:26 -0400

    USe delayed numpy imports

--- clouseau/visualize.py
+++ clouseau/visualize.py
@@ -1,18 +1,21 @@
 from typing import Any
 
-import numpy as np
 from rich.console import Console
 from rich.tree import Tree
 
 FORMATTER_REGISTRY = {}
 
+
 class LeaveFormatter:
     """"""
+
     pass
 
 
 def array_values(value, color="cornsilk1"):
     """Print array stats"""
+    import numpy as np
+
     with np.printoptions(precision=2, edgeitems=2, threshold=100):
         str_value = str(value)
 
@@ -21,6 +24,8 @@ def array_values(value, color="cornsilk1"):
 
 def array_stats(value, color="pale_turquoise1", color_label="turquoise2"):
     """Print array stats"""
+    import numpy as np
+
     str_value = f"[{color_label}]mean[/{color_label}]={np.mean(value):.2e}, [{color_label}]std[/{color_label}]={np.std(value):.2e}, "
     str_value += f"[{color_label}]min[/{color_label}]={np.min(value):.2e}, [{color_label}]max[/{color_label}]={np.max(value):.2e}"
     return f"[{color}]{str_value}[/{color}]" + array_values(value)
@@ -50,4 +55,6 @@ def _add_dict_to_tree(parent_node: Tree, data: dict[str, Any], format_leave) -> 
         else:
             # Add leaf node for primitive values
             formatted_value = format_leave(value)
-            parent_node.add(f"[dark_turquoise]{key} {value.dtype.str}({value.shape})[/dark_turquoise]: {formatted_value}")
+            parent_node.add(
+                f"[dark_turquoise]{key} {value.dtype.str}({value.shape})[/dark_turquoise]: {formatted_value}"
+            )
