1bb458f66436f3b1fd0807441be9c326e85d3681
Axel Donath <adonath@users.noreply.github.com>
2025-08-15 15:40:36 -0400

    Update torch_utils.py

--- clouseau/torch_utils.py
+++ clouseau/torch_utils.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from typing import Any
+import logging
 
 import torch
 from torch import nn
@@ -8,13 +9,19 @@ from .io_utils import PATH_SEP
 
 CACHE: dict[str, list[torch.Tensor]] = {}
 
+log = logging.getLogger(__file__)
+
 
 def add_to_cache_torch(key: str) -> Callable:
     """Add a intermediate x to the global cache"""
 
     def hook(module: nn.Module, input_: Any, output: torch.Tensor) -> None:
-        key_full = key + PATH_SEP + "forward"
+        key_full = key + PATH_SEP + "__call__"
         CACHE.setdefault(key_full, [])
+        if isinstance(output, tuple):
+            log.warning(f"Output for `{key_full}` is a tuple, choosing first entry only.")
+            output = output[0]
+        
         CACHE[key_full].append(output.detach().clone())
 
     return hook
